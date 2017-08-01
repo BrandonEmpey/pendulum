8cffa78cda85546130f81dd53bc2b19f28cbd0da
Anne Truong <truonganne96@gmail.com>
2017-08-01 17:11:25 +0200

    Commit this

--- models/user.js
+++ models/user.js
@@ -3,13 +3,11 @@ const {Schema} = mongoose;
 
 
 const userSchema = new Schema({
-  _id: Number,
   username: String,
   password: String,
   firstName: String,
   lastName: String,
   email: String,
-  nickname: String,
   gradeIds: [{ type: Schema.Types.ObjectId, ref: 'Grade'}],
   commentIds: [{ type: Schema.Types.ObjectId, ref: 'Comment'}]
 });
